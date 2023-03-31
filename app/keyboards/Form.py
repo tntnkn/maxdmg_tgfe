8faf8f27714e85c4c6fcd110454b54f4f704b683
no <no>
2023-03-31 23:13:45 +0300

    FIX: forms are now checked for visibiliy before being displayed/hid.

--- app/keyboards/Form.py
+++ app/keyboards/Form.py
@@ -2,6 +2,7 @@ class Form():
     def __init__(self, tg_user_id):
         self.tg_user_id = tg_user_id
         self.groups     = list()
+        self.is_visible = False
 
     def AddGroup(self, group):
         if not group:
@@ -9,10 +10,21 @@ class Form():
         self.groups.append(group)
 
     async def Display(self):
+        if self.IsVisible():
+            return
         for group in self.groups:
             await group.Display()
+        else:
+            self.is_visible = True
 
     async def Hide(self):
+        if not self.IsVisible():
+            return
         for group in self.groups:
             await group.Hide()
+        else:
+            self.is_visible = False
+
+    def IsVisible(self):
+        return self.is_visible
 

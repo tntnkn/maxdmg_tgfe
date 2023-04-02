8c119995b268e7c5a081a7e82114eac7d4d1a1e7
no <no>
2023-04-02 13:24:16 +0300

    FIX: safer messaging.

--- app/Utils/MessageManager.py
+++ app/Utils/MessageManager.py
@@ -1,29 +1,59 @@
 from aiogram.types  import ParseMode 
+from aiogram.utils.exceptions import (
+    MessageToDeleteNotFound,
+    BotBlocked,
+)
 from ..bot          import bot
 from ..Static       import Vocabulary as Voc
+from ..Loggers      import DefaultLogger
+from ..Exceptions   import TGFE_BadUserInSystem
 
+
+__logger  = DefaultLogger()
 
 class MessageManager():
     @staticmethod
     async def SendText(text, tg_user_id):
-        resp = await bot.send_message(
-                 tg_user_id, 
-                 text,
-                 parse_mode=ParseMode.MARKDOWN)
-        return resp.message_id
+        try:
+            resp = await bot.send_message(
+                     tg_user_id, 
+                     text,
+                     parse_mode=ParseMode.MARKDOWN)
+            return resp.message_id
+        except BotBlocked:
+            __logger(f"\nCannot send text {text} -- user {tg_user_id} blocked the bot!",
+                     exc_info=True)
+            raise TGFE_BadUserInSystem("In MessageManager.SendText", 
+                                      tg_user_id)
 
     @staticmethod
     async def SendInlineKB(kb, kb_desc, tg_user_id):
-        resp = await bot.send_message(
-                 tg_user_id, 
-                 kb_desc,
-                 reply_markup=kb,
-                 parse_mode=ParseMode.MARKDOWN)
-        return resp.message_id
+        try:
+            resp = await bot.send_message(
+                     tg_user_id, 
+                     kb_desc,
+                     reply_markup=kb,
+                     parse_mode=ParseMode.MARKDOWN)
+            return resp.message_id
+        except BotBlocked:
+            __logger(f"\nCannot send inline KB {kb_desc} -- user {tg_user_id} blocked the bot!",
+                     exc_info=True)
+            raise TGFE_BadUserInSystem("In MessageManager.SenfInlineKB", 
+                                      tg_user_id)
 
     @staticmethod
     async def Delete(message_id, tg_user_id):
-        return await bot.delete_message(tg_user_id, message_id)
+        try:
+            return await bot.delete_message(tg_user_id, message_id)
+        except MessageToDeleteNotFound:
+            __logger.error(f"\nCannot delete message {message_id} for {tg_user_id}",
+                         exc_info=True)
+            return None
+        except BotBlocked:
+            __logger(f"\nCannot delete message {message_id} -- user {tg_user_id} blocked the bot!",
+                     exc_info=True)
+            raise TGFE_BadUserInSystem("In MessageManager.Delete", 
+                                      tg_user_id)
 
     @staticmethod
     async def SendDocument(document, tg_user_id):

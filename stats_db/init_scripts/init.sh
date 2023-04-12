1f5725aeb9915e708f0dff7c1faa1c3a72a3a0da
no <no>
2023-04-12 20:29:20 +0300

    ADD: `tgfe_stats_db` now have a reader with `SELECT` only privilidges.

--- stats_db/init_scripts/init.sh
+++ stats_db/init_scripts/init.sh
@@ -7,6 +7,9 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
 
 	CREATE USER tgfe_stats_admin;
     ALTER USER tgfe_stats_admin PASSWORD '$TGFE_STATS_ADMIN_PASS';
+
+    CREATE USER tgfe_stats_reader;
+    ALTER USER tgfe_stats_reader PASSWORD '$TGFE_STATS_READER_PASS';
 EOSQL
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname tgfe_stats_db <<-EOSQL
@@ -16,4 +19,13 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname tgfe_stats_db <<-EO
     ALTER DEFAULT PRIVILEGES 
         FOR ROLE tgfe_stats_admin
         GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO tgfe_stats_admin;
+
+    REVOKE ALL PRIVILEGES ON DATABASE tgfe_stats_db FROM tgfe_stats_reader;
+    ALTER DEFAULT PRIVILEGES 
+        FOR ROLE tgfe_stats_admin
+        REVOKE ALL ON TABLES FROM tgfe_stats_reader;
+    GRANT CONNECT ON DATABASE tgfe_stats_db TO tgfe_stats_reader;
+    ALTER DEFAULT PRIVILEGES 
+        FOR ROLE tgfe_stats_admin
+        GRANT SELECT ON TABLES TO tgfe_stats_reader;
 EOSQL
